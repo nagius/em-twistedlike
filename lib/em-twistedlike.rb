5b280e9719a106945f65a6fbd6633242526b286d
Nicolas Agius <nicolas.agius@lps-it.fr>
2015-11-10 18:01:08 +0000

    Added copyleft

--- lib/em-twistedlike.rb
+++ lib/em-twistedlike.rb
@@ -1,3 +1,25 @@
+# -*- coding:utf-8 -*-
+
+# em-twistedlike - Implement some of Twisted's features within EventMachine
+# Copyleft 2015 - Nicolas AGIUS <nicolas.agius@lps-it.fr>
+
+###########################################################################
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###########################################################################
+
 require 'eventmachine'
 
 # This Monkeypatch implement some of Twisted's defer funtionnalities to EventMachine Deferrable
